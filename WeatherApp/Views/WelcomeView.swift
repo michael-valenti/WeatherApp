3c16b80ec51b09fdfbfb7cf3fe17bdfd09418d5f
michael-valenti <mvalenti14@live.com>
2024-09-03 17:09:45 -0400

    Created WelcomeView. Call WelcomeView from the ContentView.

---
+++ WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Mike Valenti on 9/3/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
