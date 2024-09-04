76c2d82ef70ee0cda41d6de9532be07e73b3bb66
michael-valenti <mvalenti14@live.com>
2024-09-04 15:34:02 -0400

    Display coordinates from users phone

--- WeatherApp/Views/ContentView.swift
+++ WeatherApp/Views/ContentView.swift
@@ -13,7 +13,19 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            WelcomeView().environmentObject(locationManager)
+            
+            if let location = locationManager.location{
+                Text("The coordinates of your location are:\(location.longitude),\(location.latitude)")
+            }
+            else {
+                if locationManager.isLoading {
+                    LoadingView()
+                }
+                else {
+                    WelcomeView().environmentObject(locationManager)
+                }
+            }
+            
         }
         
         .background(Color(hue: 0.685, saturation: 1.0, brightness: 0.995))
