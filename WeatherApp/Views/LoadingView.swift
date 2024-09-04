c01e8df9107c42e553b3853578fb3ddd01675a22
michael-valenti <mvalenti14@live.com>
2024-09-04 15:32:34 -0400

    Weather Manager created to handle API request

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Mike Valenti on 9/3/24.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
